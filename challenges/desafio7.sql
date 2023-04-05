a8dc665efa1d543a2f687c0191f08da675e873a2
Marlon Couto <marloncouto199@gmail.com>
2023-04-05 22:11:24 +0000

    fix: corrige exibicao dos dados no arquivo desafio7.sql

--- challenges/desafio7.sql
+++ challenges/desafio7.sql
@@ -4,7 +4,7 @@ SELECT
   COUNT(seg.pessoa_usuaria_id) AS pessoas_seguidoras
 FROM
   artista AS art
-  INNER JOIN album AS alb ON art.artista_id = alb.album_id
+  INNER JOIN album AS alb ON art.artista_id = alb.artista_id
   INNER JOIN seguindo_artista AS seg ON seg.artista_id = art.artista_id
 GROUP BY
   artista,
